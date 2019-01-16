07b6a4baf92aa562ee786275c2cf35e9d416b581
Pi2f <pierre.franc.gap@gmail.com>
2019-01-16 15:55:37 +0100

    update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const uuidv4 = require('uuid/v4');
 module.exports = {
-    'urlDB' : 'mongodb://192.168.99.100:32768/Users',
-    // 'urlDB' : 'mongodb://tu-streames1:tu-streames1@ds153974.mlab.com:53974/tu-streames',
+    // 'urlDB' : 'mongodb://192.168.99.100:32768/Users',
+    'urlDB' : 'mongodb://tu-streames1:tu-streames1@ds153974.mlab.com:53974/tu-streames',
     // 'urlDB' : 'mongodb://database/TuStreames',
     'port' : "3001",
 }
